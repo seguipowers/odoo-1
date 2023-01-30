20413827b0ae5da4f952995292c92e0c9327e105
Juan José Seguí Borja <seguipowers@gmail.com>
2023-01-30 01:15:10 +0100

    Actualización final

--- custom_addons/custom_crm/controllers/controllers.py
+++ custom_addons/custom_crm/controllers/controllers.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+import json
+from odoo.http import Response
 
+class VisitController(http.Controller):
 
-# class CustomCrm(http.Controller):
-#     @http.route('/custom_crm/custom_crm', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/custom_crm/custom_crm/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('custom_crm.listing', {
-#             'root': '/custom_crm/custom_crm',
-#             'objects': http.request.env['custom_crm.custom_crm'].search([]),
-#         })
-
-#     @http.route('/custom_crm/custom_crm/objects/<model("custom_crm.custom_crm"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('custom_crm.object', {
-#             'object': obj
-#         })
+    @http.route('/api/visits', auth='public', method=['GET'], csrf=False)
+    def get_visits(self,**kw): 
+        try: 
+            visits = http.request.env['custom_crm.visit'].sudo().search_read([], ['id','name','customer', 'done']) 
+            res = json.dumps(visits, ensure_ascii=False).encode('utf-8')
+            return Response(res, content_type='applicacion/json;charset=utf-8', status=200)
+        except Exception as e:
+            return Response(json.dumps({'error':str(e)}), content_type='applicacion/json;charset=utf-8', status=505)
+            
--- custom_addons/custom_crm/models/models.py
+++ custom_addons/custom_crm/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import datetime
 from odoo import models, fields, api
 
 
@@ -12,3 +13,48 @@ class Visit(models.Model):#Especificamos el tipo de modulo (Model es el típico 
      date = fields.Datetime(string='fecha')
      type = fields.Selection([('p', 'Presencial'),('w','Whatsapp'),('t','telefónico')], string='Tipo', required=True)
      done = fields.Boolean(string='Realizada', readonly=True)
+     image = fields.Binary(string='Image')
+
+     def toggle_state(self):
+          self.done = not self.done
+
+
+     def f_create(self):
+          visit = {
+               'name' : 'ORM test',
+               'customer' : 1,
+               'date':str(datetime.date(2023,2,2)),
+               'type':'p',
+               'done':False
+          }     
+          print(visit)
+          self.env['custom_crm.visit'].create(visit)
+
+     def f_search_update(self):
+          visit = self.env['custom_crm_visit'].search([('name','*','ORM test')])
+          print('search()', visit, visit.name)
+
+          visit_b = self.env['custom_crm_visit'].browse([8])
+          print('browse()', visit_b, visit_b.name)
+
+          visit.write({
+               'name':'ORM test write'
+          })
+
+     def f_delete(self):     
+          visit = self.env['custom_crm_visit'].browse([8])
+          visit.unlink()
+
+class VisitReport(models.AbtractModel):
+
+     _name='report.custom_crm.visit.card'
+
+     @api.model
+     def _get_report_values(self,docids,data=None):
+          report_obj = self.env['ir.actions.report']
+          report = report_obj._get_report_from_name('report.custom_crm.visit.card')
+          return {
+               'doc_ids':docids,
+               'doc_model':self.env['custom_crm.visit'],
+               'docs': self.env['custom_crm.visit'].browse(docids)
+          }
